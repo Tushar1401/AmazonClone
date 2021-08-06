c8186d5462444aa174dcf7ec90ac1c1437c3d5d4
Tushar1401 <tushararora1401@gmail.com>
2021-08-06 12:47:55 +0530

    added header and home components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="app">
-      <h1>HELLO</h1>
+
+      {/* Header */}
+      <Header />
+      {/* Home */}
+      <Home />
+
     </div>
   );
 }
